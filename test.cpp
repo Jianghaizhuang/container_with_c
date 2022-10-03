dde78ce2607eb278ee888a1181e41912810ba28f
Jianghaizhuang <72744907+Jianghaizhuang@users.noreply.github.com>
2022-10-03 18:33:32 +0800

    Delete test.cpp

--- test.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main(){
-  for (int i = 0; i < 10; i++) {
-    cout << "new hello world" << endl;
-  }
-  cout << "hello world !! " << endl;
-  return 0;
-}
