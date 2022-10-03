3f9fdab3c444528ed92fc28f442969d7ed4f8b2d
Jianghaizhuang <1330559844@qq.com>
2022-10-03 17:36:43 +0800

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+  for (int i = 0; i < 10; i++) {
+    cout << "new hello world" << endl;
+  }
+  cout << "hello world !! " << endl;
+  return 0;
+}
